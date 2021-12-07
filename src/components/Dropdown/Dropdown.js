885c4693511c4cd46c1e0c5f279aed27fa7f554f
AeeUX <AndreasEriksen99@hotmail.com>
2021-12-07 14:52:12 +0100

    Update Dropdown.js

---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from 'react'
+
+function Dropdown(props) {
+  const dropdownRef = useRef()
+
+  const handleClick = (event) => {
+    if (
+      dropdownRef &&
+      !dropdownRef.current?.contains(event.target) &&
+      props.onClose
+    )
+      props.onClose()
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', handleClick)
+
+    return () => {
+      document.removeEventListener('click', handleClick)
+    }
+  })
+
+  return (
+    <div
+      ref={dropdownRef}
+      className={`dropdown custom-scroll ${props.class ? props.class : ''}`}
+    >
+      {props.children}
+    </div>
+  )
+}
+
+export default Dropdown
