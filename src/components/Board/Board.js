adadf06cfd7b115918e1bc905fc0cf2b8f9e358f
AeeUX <AndreasEriksen99@hotmail.com>
2021-12-08 00:19:49 +0100

    Update Board.js

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
 import { MoreHorizontal } from 'react-feather'
+import styled from 'styled-components'
+import { motion, AnimatePresence } from 'framer-motion'
+
+import './Board.css'
 
 import Card from '../Card/Card'
 import Dropdown from '../Dropdown/Dropdown'
@@ -9,7 +13,7 @@ function Board(props) {
   const [showDropdown, setShowDropdown] = useState(false)
 
   return (
-    <div className="board bg-board-color px-4 py-3 rounded-lg">
+    <div className="board">
       <div className="board_header cursor-not-allowed flex flex-row">
         <p className="board_header_title">
           {props.board?.title}
@@ -25,33 +29,48 @@ function Board(props) {
               class="board_dropdown"
               onClose={() => setShowDropdown(false)}
             >
-              <p className="cursor-pointer" onClick={() => props.removeBoard()}>Delete Board</p>
+              <p className="cursor-pointer" onClick={() => props.removeBoard()}>
+                Delete Board
+              </p>
             </Dropdown>
           )}
         </div>
       </div>
-      <div className="board_cards cursor-pointer custom-scroll">
-        {props.board?.cards?.map((item) => (
-          <Card
-            key={item.id}
-            card={item}
-            boardId={props.board.id}
-            removeCard={props.removeCard}
-            dragEntered={props.dragEntered}
-            dragEnded={props.dragEnded}
-            updateCard={props.updateCard}
-          />
-        ))}
+      <Container className="board_cards cursor-pointer custom-scroll">
         <Editable
-          text="+ Add Card"
+          text="+"
           placeholder="Enter Card Title"
           displayClass="board_add-card"
           editClass="board_add-card_edit"
           onSubmit={(value) => props.addCard(props.board?.id, value)}
         />
-      </div>
+        {props.board?.cards?.map((item) => (
+          <TaskItem>
+            <Card
+              key={item.id}
+              card={item}
+              boardId={props.board.id}
+              removeCard={props.removeCard}
+              dragEntered={props.dragEntered}
+              dragEnded={props.dragEnded}
+              updateCard={props.updateCard}
+            />
+          </TaskItem>
+        ))}
+      </Container>
     </div>
   )
 }
 
 export default Board
+
+const Container = styled.div``
+
+const TaskItem = styled.div`
+  background-color: whitesmoke;
+  border-radius: 5px;
+  padding: 3rem 3rem 4.5rem;
+  grid-gap: 1.5rem;
+  display: grid;
+  justify-content: center;
+`
