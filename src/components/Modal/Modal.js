d2e292a402e11cbd43fe90160c204428fbe4a1d1
AeeUX <AndreasEriksen99@hotmail.com>
2021-12-08 16:14:50 +0100

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,19 +1,41 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Modal(props) {
   return (
-    <div
+    <ModalItem
       className="modal"
       onClick={() => (props.onClose ? props.onClose() : '')}
     >
-      <div
+      <ModalContent
         className="modal_content custom-scroll"
         onClick={(event) => event.stopPropagation()}
       >
         {props.children}
-      </div>
-    </div>
+      </ModalContent>
+    </ModalItem>
   )
 }
 
 export default Modal
+
+const ModalItem = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  min-height: 100vh;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.46);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 20;
+`
+const ModalContent = styled.div`
+  overflow-y: auto;
+  max-height: 95vh;
+  background-color: #fff;
+  border-radius: 3px;
+  box-shadow: 1px 1px 20px rgba(0, 0, 0, 0.12);
+`
