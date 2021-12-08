62e0d16bbce2913383a1cb5e3627bdeab1433b6c
AeeUX <AndreasEriksen99@hotmail.com>
2021-12-08 16:14:39 +0100

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { CheckSquare, Clock, MoreHorizontal } from 'react-feather'
+import styled from 'styled-components'
 
 import Dropdown from '../Dropdown/Dropdown'
 
@@ -46,29 +47,29 @@ function Card(props) {
           updateCard={props.updateCard}
         />
       )}
-      <div
-        className="card"
+      <CardItem
+        className=""
         draggable
         onDragEnd={() => props.dragEnded(props.boardId, id)}
         onDragEnter={() => props.dragEntered(props.boardId, id)}
         onClick={() => setShowModal(true)}
       >
-        <div className="card_top">
-          <div className="card_top_labels">
+        <CardTop className="">
+          <CardTopLabels className="">
             {labels?.map((item, index) => (
-              <label key={index} style={{ backgroundColor: item.color }}>
+              <Label key={index} style={{ backgroundColor: item.color }}>
                 {item.text}
-              </label>
+              </Label>
             ))}
-          </div>
-          <div
-            className="card_top_more"
+          </CardTopLabels>
+          <CardTopMore
+            className=""
             onClick={(event) => {
               event.stopPropagation()
               setShowDropdown(true)
             }}
           >
-            <MoreHorizontal />
+            <MoreHorizontal className="" />
             {showDropdown && (
               <Dropdown
                 class="board_dropdown"
@@ -79,26 +80,89 @@ function Card(props) {
                 </p>
               </Dropdown>
             )}
-          </div>
-        </div>
-        <div className="card_title">{title}</div>
-        <div className="card_footer">
+          </CardTopMore>
+        </CardTop>
+        <CardTitle className="">{title}</CardTitle>
+        <CardFooter className="">
           {date && (
-            <p className="card_footer_item">
-              <Clock className="card_footer_icon" />
+            <CardFooterItem className="">
+              <Clock className="" />
               {formatDate(date)}
-            </p>
+            </CardFooterItem>
           )}
           {tasks && tasks?.length > 0 && (
-            <p className="card_footer_item">
-              <CheckSquare className="card_footer_icon" />
+            <CardFooterItem className="">
+              <CheckSquare className="h-13 w-13" />
               {tasks?.filter((item) => item.completed)?.length}/{tasks?.length}
-            </p>
+            </CardFooterItem>
           )}
-        </div>
-      </div>
+        </CardFooter>
+      </CardItem>
     </>
   )
 }
 
 export default Card
+
+const CardItem = styled.div`
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  background-color: #fff;
+  border-radius: 10px;
+  &hover {
+    opacity: 1;
+  }
+`
+const CardTop = styled.div`
+  display: flex;
+  align-items: flex-start;
+`
+const CardTopLabels = styled.div`
+  flex: 3;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+  font-size: 14px;
+  line-height: 21px;
+`
+const Label = styled.div`
+  border-radius: 40px;
+  padding: 4px 12px;
+  background-color: gray;
+  color: #fff;
+  width: fit-content;
+`
+const CardTopMore = styled.div`
+  width: 30px;
+  height: 20px;
+  transform: translateX(15px);
+  flex: 1;
+  cursor: pointer;
+  opacity: 1;
+  transition: 200ms;
+`
+const CardTitle = styled.div`
+  flex: 1;
+  font-weight: bold;
+  font-size: 1rem;
+  line-height: 1.875rem;
+`
+const CardFooter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+const CardFooterItem = styled.div`
+  border-radius: 40px;
+  padding: 4px 12px;
+  background-color: #f8f8f8;
+  color: #000;
+  width: fit-content;
+  font-size: 14px;
+  line-height: 21px;
+  display: flex;
+  gap: 5px;
+  align-items: center;
+`
