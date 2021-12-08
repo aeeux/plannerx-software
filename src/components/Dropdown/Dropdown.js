c216762bd5f5aaef129cfc75a9ef01f1572831d8
AeeUX <AndreasEriksen99@hotmail.com>
2021-12-08 16:14:44 +0100

    Update Dropdown.js

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from 'react'
+import styled from 'styled-components'
 
 function Dropdown(props) {
   const dropdownRef = useRef()
@@ -21,13 +22,29 @@ function Dropdown(props) {
   })
 
   return (
-    <div
+    <DropownItem
       ref={dropdownRef}
-      className={`dropdown custom-scroll ${props.class ? props.class : ''}`}
+      className={`custom-scroll ${props.class ? props.class : ''}`}
     >
       {props.children}
-    </div>
+    </DropownItem>
   )
 }
 
 export default Dropdown
+
+const DropownItem = styled.div`
+  position: absolute;
+  right: 0;
+  top: 100%;
+  background-color: #fff;
+  border-radius: 3px;
+  min-height: 40px;
+  min-width: 80px;
+  width: fit-content;
+  height: fit-content;
+  max-width: 250px;
+  max-height: 390px;
+  overflow-y: auto;
+  z-index: 5;
+`
