7adcb259d4895bc5c8f09ad7ace5efa7f40375f9
AeeUX <AndreasEriksen99@hotmail.com>
2021-12-08 16:14:47 +0100

    Update Editable.js

--- src/components/Editable/Editable.js
+++ src/components/Editable/Editable.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 
 import { X } from 'react-feather'
+import './Editable.css'
 
 function Editable(props) {
   const [isEditable, setIsEditable] = useState(false)
@@ -16,23 +18,32 @@ function Editable(props) {
   }
 
   return (
-    <div className="editable">
+    <EditableItem className="editable">
       {isEditable ? (
         <form
           className={`editable_edit ${props.editClass ? props.editClass : ''}`}
           onSubmit={submission}
         >
           <input
+            className="b-2 border-solid rounded-lg p-10 border-blue-500 outline-none"
             type="text"
             value={inputText}
             placeholder={props.placeholder || props.text}
             onChange={(event) => setInputText(event.target.value)}
             autoFocus
           />
-          <div className="editable_edit_footer">
-            <button type="submit">{props.buttonText || 'Add'}</button>
-            <X onClick={() => setIsEditable(false)} className="closeIcon" />
-          </div>
+          <EditableFooter className="">
+            <button
+              className="cursor-pointer border-solid rounded-lg p-3 bg-blue-500 outline-none text-white transition ease-in-out hover:bg-blue-200"
+              type="submit"
+            >
+              {props.buttonText || 'Add'}
+            </button>
+            <X
+              onClick={() => setIsEditable(false)}
+              className="w-10 cursor-pointer"
+            />
+          </EditableFooter>
         </form>
       ) : (
         <p
@@ -44,8 +55,18 @@ function Editable(props) {
           {props.text}
         </p>
       )}
-    </div>
+    </EditableItem>
   )
 }
 
 export default Editable
+
+const EditableItem = styled.div`
+  width: 100%;
+`
+
+const EditableFooter = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+`
