3cb991585b79ca29e849e28df2a743fd91ff01c9
AeeUX <AndreasEriksen99@hotmail.com>
2021-12-09 22:14:12 +0100

    Update ToggleSwitch.js

--- src/ToggleSwitch.js
+++ src/ToggleSwitch.js
@@ -1,14 +1,54 @@
-import React, { useState } from "react";
-import "./App.css";
+import styled from 'styled-components'
+import { CgSun } from 'react-icons/cg'
+import { HiMoon } from 'react-icons/hi'
 
-function ToggleSwitch() {
-  const [isToggled, setIsToggled] = useState(false);
-  const onToggle = () => setIsToggled(!isToggled);
+function ToggleSwitch(props) {
+  function changeTheme() {
+    if (props.theme === 'light') {
+      props.setTheme('dark')
+    } else {
+      props.setTheme('light')
+    }
+  }
+
+  const icon =
+    props.theme === 'light' ? <HiMoon size={40} /> : <CgSun size={40} />
+
   return (
-    <label className="toggle-switch">
-      <input type="checkbox" checked={isToggled} onChange={onToggle} />
-      <span className="switch" />
-    </label>
-  );
+    <Page>
+      <Container>
+        <Toggle onClick={changeTheme}>{icon}</Toggle>
+      </Container>
+    </Page>
+  )
 }
-export default ToggleSwitch;
+
+export default ToggleSwitch
+
+const Toggle = styled.button`
+  cursor: pointer;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  border: none;
+  background-color: ${(props) => props.theme.titleColor};
+  color: ${(props) => props.theme.titleColor};
+  &:focus {
+    outline: none;
+  }
+  transition: all 0.5s ease;
+`
+
+const Page = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.pageBackground};
+  transition: all 0.5s ease;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
