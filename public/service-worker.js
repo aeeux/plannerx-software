c552b68a73e00fe94dc9445a75b91ae3cc902377
AeeUX <AndreasEriksen99@hotmail.com>
2021-12-15 14:47:05 +0100

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js',
+)
+
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === 'image',
+  new workbox.strategies.CacheFirst(),
+)
+
+workbox.routing.registerRoute(
+  new RegExp('/.*'),
+  new workbox.strategies.NetworkFirst(),
+)
